1e59afc3169f47387cd17b31f18b307605046154
DaniqueJ-R <24005981@student.peterborough.ac.uk>
2025-09-04 22:40:10 +0000

    testing settings for cloud images

--- codestar/settings.py
+++ codestar/settings.py
@@ -158,7 +158,7 @@ USE_TZ = True
 STATIC_URL = "static/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
+DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
